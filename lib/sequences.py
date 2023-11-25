4ac47778d13df4b59fb76f15ada55d199d6eafc8
isaacgichuhi <zacmuturi45@gmail.com>
2023-11-25 03:07:18 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        x = 2
+        my_list = [0, 1]
+        while x < length:
+            my_list.append(my_list[x-1] + my_list[x-2])
+            x += 1
+        print(my_list)
+
+
+
+
+
